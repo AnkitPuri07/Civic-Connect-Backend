dce98e6995b68dd51496ca36e6d20294c0b9774e
Ankit Puri <your-email@example.com>
2026-05-17 13:58:50 +0545

    Initialized project for backend of civic connect

---
+++ src/main/java/com/ankit/civicconnectbackend/controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.ankit.civicconnectbackend.controllers;
+
+public class UserController {
+}
---
+++ src/test/java/com/ankit/civicconnectbackend/CivicConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.civicconnectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicConnectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
